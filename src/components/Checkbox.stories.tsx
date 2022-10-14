a8afcb7e6b8f596d058aab6189b7f8a26717bf98
MICKAEL RANGEL <mickaelsrangel@gmail.com>
2022-10-13 22:48:21 -0300

    Initial commit

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,24 @@
+import { Checkbox, CheckboxProps } from './Checkbox';
+import { Meta, StoryObj } from '@storybook/react';
+import { Text } from './Text';
+
+export default {
+    title: 'Components/Checkbox',
+    component: Checkbox,
+    args: {},
+    argTypes: {},
+    decorators: [
+        (Story) => {
+            return (
+                <div className='flex items-center gap-2'>
+                    {Story()}
+                    <Text size='sm'>
+                        Text for checobox.
+                    </Text>
+                </div>
+            );
+        }
+    ]
+} as Meta<CheckboxProps>;
+
+export const Default: StoryObj<CheckboxProps> = {}
